2e8266a3c171a9f9796739e7193667ce0c39b124
Zechariah-Angus <zechariah.mcdougall@dcmail.ca>
2025-12-01 19:58:48 -0500

    Added constants to story context
    Added constants to story text

--- Lab 4 - JavaScript/Part 1/script.js
+++ Lab 4 - JavaScript/Part 1/script.js
@@ -41,9 +41,10 @@ function returnRandomStoryString() {
     const storyText = 
 
    "It was 94 Fahrenheit outside, so " 
-   + insertx + " went for a walk. When they got to "
-   + inserty + ", they stared in horror for a few moments, then "
-   + insertz + ". Bob saw the whole thing, but was not surprised — "+ insertx + " weighs 300 pounds, and it was a hot day."
+   + randomCharacter + " went for a walk. When they got to "
+   + randomPlaces + ", they stared in horror for a few moments, then "
+   + randomEvents + ". Bob saw the whole thing, but was not surprised — "
+   + randomCharacter + " weighs 300 pounds, and it was a hot day."
 
   return storyText;
 }
